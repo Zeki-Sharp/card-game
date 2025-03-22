aea8e542328681f26e61664a35dfaf19fe731d39
Zeki-Sharp <571258652@qq.com>
2025-03-22 23:08:19 +0800

    修改了数据提交的方法

--- Assets/Scripts/Model/CardData.cs
+++ Assets/Scripts/Model/CardData.cs
@@ -5,25 +5,35 @@ namespace ChessGame
     [System.Serializable]
     public class CardData
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-        public int Attack { get; set; }
-        public int Health { get; set; }
-        public Sprite Image { get; private set; }
-
+        [SerializeField] private int id;
+        [SerializeField] private string name;
+        [SerializeField] private int attack;
+        [SerializeField] private int health;
+        [SerializeField] private Sprite image;
+        
+        public int Id => id;
+        public string Name => name;
+        public int Attack { get => attack; set => attack = value; }
+        public int Health { get => health; set => health = value; }
+        public Sprite Image => image;
+        
+        // 默认构造函数，用于序列化
+        public CardData() { }
+        
+        // 带参数的构造函数
         public CardData(int id, string name, int attack, int health, Sprite image)
         {
-            Id = id;
-            Name = name;
-            Attack = attack;
-            Health = health;
-            Image = image;
+            this.id = id;
+            this.name = name;
+            this.attack = attack;
+            this.health = health;
+            this.image = image;
         }
-
+        
         // 创建卡牌数据的副本
         public CardData Clone()
         {
-            return new CardData(Id, Name, Attack, Health, Image);
+            return new CardData(id, name, attack, health, image);
         }
     }
 } 
